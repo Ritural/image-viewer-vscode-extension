923e98caa2b4cfa1d50e61ebf13c612586b48e98
Kallan Jones <kallan@alphero.com>
2020-03-17 08:46:18 +1300

    Update eslint and tsconfig based on boilerplate.

--- eslintrc.js
+++ eslintrc.js
@@ -1,3 +1,18 @@
-{
-  
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  plugins: ["@typescript-eslint"],
+  env: {
+    node: true,
+  },
+  extends: [],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    // ecmaFeatures: {
+    //   jsx: true
+    // }
+  },
+  rules: {
+    
+  }
 }
