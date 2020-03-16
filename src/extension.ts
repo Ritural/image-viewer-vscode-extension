a78683a94896adc1f8e492efe4f0d6ea7c33a986
Kallan Jones <kallan@alphero.com>
2020-03-17 08:46:53 +1300

    Rename command in package.json and extension.ts.

--- src/extension.ts
+++ src/extension.ts
@@ -7,16 +7,18 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('<image-viewer-vscode-extension> is now active!');
+	console.log('🧐🧐🧐 <image-viewer-vscode-extension> is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.imageInsert', () => {
+	let disposable = vscode.commands.registerCommand('extension.viewProjectImages', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Looking for image');
+		vscode.window.showInformationMessage('Hello World!');
+
+    vscode.window.showInformationMessage('TEST TEST!');
 	});
 
 	context.subscriptions.push(disposable);
